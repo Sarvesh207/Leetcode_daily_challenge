d4f3a40b17dc2bf6e488da5040eab9077ec43028
Sarvesh207 <sarveshgaynar@gmail.com>
2023-12-17 11:22:47 +0530

    completed todays chellange

---
+++ December2023/2665.Counter2/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+
+    let increment = function () {
+        return ++count
+    }
+
+    let decrement = function () {
+        return --count;
+    }
+
+    let reset = function () {
+        return (count = init);
+    }
+
+    return {increment, decrement, reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
